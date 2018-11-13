42fae1982bbbe1970081ed3c2a038917e73f3ff6
Rohit Bose <rohit.kr.bose@gmail.com>
2018-11-14 03:16:30 +0530

    Working maybe

--- videochat/experimental.py
+++ videochat/experimental.py
@@ -1,36 +1,35 @@
 import socket, videosocket
 from videofeed import VideoFeed
-import threading
+from threading import Thread
 import time
 import StringIO
 
 occupied = False
-zoom = False
-videofeed = VideoFeed(1,"axax",1)
-U_client_socket = ''
-U_address = ''
+haveConnection = False
+U_client_socket = U_address = None
 
 class Daemon:
     def __init__(self):
         self.daemon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.daemon_socket.bind(("", 6001))
         self.daemon_socket.listen(5)
-        print "TCPServer Waiting for client on port 6000"
+        print "TCPServer Waiting for client on port 6001"
 
     def start(self):
+        global haveConnection, U_client_socket, U_address
         while True:
             cs, addr = self.daemon_socket.accept()
             if (occupied == True):
             	continue
-            zoom = True
+            haveConnection = True
             U_client_socket = cs; U_address = addr;
             print "I got a connection from ", addr
-            break
 
 class Server:
     def start(self):
         client_socket, address = U_client_socket, U_address
         vsock = videosocket.videosocket(client_socket)
+        videofeed = VideoFeed(1," ",1)
         while True:
             frame = vsock.vreceive()
             videofeed.set_frame(frame)
@@ -38,15 +37,12 @@ class Server:
             vsock.vsend(frame)
 
 class Client:
-    def __init__(self, ip_addr = "127.0.0.1"):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((ip_addr, 6000))
-        self.vsock = videosocket.videosocket (self.client_socket)
-        self.data = StringIO.StringIO()
-
-    def connect(self):
+    def connect(self, ip_addr = "127.0.0.1"):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((ip_addr, 6000))
+        vsock = videosocket.videosocket (client_socket)
+        videofeed = VideoFeed(1," ",1)
         while True:
-            print ('asdasd')
             frame=videofeed.get_frame()
             self.vsock.vsend(frame)
             frame = self.vsock.vreceive()
@@ -55,15 +51,14 @@ class Client:
 
 if __name__ == "__main__":
     daemon = Daemon()
-    thread_daemon = threading.Thread(target=daemon.start)
+    thread_daemon = Thread(target=daemon.start)
     thread_daemon.start()
     server = Server()
+    client = Client()
     while (True):
-        if (zoom == True):
+        if (haveConnection == True): # daemon listened to some shit
             server.start()
-            break
-    	time.sleep(5)
     	ip_addr = raw_input('Which IP do you want to connect to?\n')
     	if (ip_addr != 'N'):
-            client = Client(ip_addr)
-            client.connect()
+            client = Client()
+            client.connect(ip_addr)
