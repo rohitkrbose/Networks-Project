16457222279fdd22923268199bd6bb7f594b3e1f
Rohit Bose <rohit.kr.bose@gmail.com>
2018-11-14 16:43:48 +0530

    Back to the basics

--- videochat/serverclient.py
+++ videochat/serverclient.py
@@ -1,59 +1,97 @@
 import socket, videosocket
 from videofeed import VideoFeed
-import threading
-import time
+from threading import Thread,Timer
+import Tkinter as tk
+import tkMessageBox
 import StringIO
 
-occupied = False
-zoom = False
-videofeed = VideoFeed(1,"axax",1)
+haveConnection = False
+U_client_socket = U_address = None
 
-class Server:
+class Daemon:
     def __init__(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind(("", 6000))
-        self.server_socket.listen(5)
-        # self.videofeed = VideoFeed(1,"server",1)
-        print "TCPServer Waiting for client on port 6000"
+        self.daemon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.daemon_socket.bind(("", 6000))
+        self.daemon_socket.listen(5)
+        print "TCPServer Waiting for client on port 6001"
 
     def start(self):
+        global haveConnection, U_client_socket, U_address
         while True:
-            client_socket, address = self.server_socket.accept()
-            if (occupied == True):
-            	continue
-            zoom = True
-            print "I got a connection from ", address
-            vsock = videosocket.videosocket(client_socket)
-            while True:
-                print ('asdasd')
+            cs, addr = self.daemon_socket.accept()
+            if (haveConnection == True): # if it is already engaged with someone, ignore the new request
+                continue
+            haveConnection = True # if it is not engaged, set this flag variable to true
+            U_client_socket = cs; U_address = addr; # store client_socket and client address in global variables, as this is a thread
+            print "I got a connection from ", addr
+
+class Server:
+    def connect(self):
+        global haveConnection, U_client_socket, U_address
+        client_socket, address = U_client_socket, U_address # retrieve info from global variables (changes made by Daemon)
+        vsock = videosocket.videosocket(client_socket) # establish a video connection
+        videofeed = VideoFeed(1,"Server",1)
+        try:
+            while (True):
                 frame = vsock.vreceive()
                 videofeed.set_frame(frame)
                 frame = videofeed.get_frame()
                 vsock.vsend(frame)
+        except:
+            print ('Exception occurred')
 
 class Client:
-    def __init__(self, ip_addr = "127.0.0.1"):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((ip_addr, 6001))
-        self.vsock = videosocket.videosocket (self.client_socket)
-        # self.videofeed = VideoFeed(1,"client",1)
-        self.data = StringIO.StringIO()
+    def connect(self, ip_addr = "127.0.0.1"):
+        global win
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client_socket.connect((ip_addr, 6001))
+        except:
+            return ('Unavailable') # if Client can't get a connection to that IP
+        win.withdraw() # Hide the Connect To window
+        vsock = videosocket.videosocket(client_socket) # establish a video connection
+        videofeed = VideoFeed(1,"Client",1)
+        try:
+            while (True):
+                frame = videofeed.get_frame()
+                vsock.vsend(frame)
+                frame = vsock.vreceive()
+                videofeed.set_frame(frame)
+        except:
+            print ('Expcetion occurred')
 
-    def connect(self):
-        while True:
-            print ('asdasd')
-            frame=videofeed.get_frame()
-            self.vsock.vsend(frame)
-            frame = self.vsock.vreceive()
-            videofeed.set_frame(frame)
+def constantlyCheck (): # I am the server! This is a helper function for the daemon.
+    global haveConnection, server, win
+    # Here we should have an option: To reject or to accept. Only accept code is written here.
+    if (haveConnection == True): # If daemon listened to some shit
+        win.withdraw()  # Hide the Connect To window
+        haveConnection = False
+        server.connect() # Initiate video chat as server
+    root.after(2, constantlyCheck) # Run this function again after 2 seconds
 
-if __name__ == "__main__":
-    server = Server()
-    thread_server = threading.Thread(target=server.start)
-    thread_server.start()
-    while (True):
-    	time.sleep(5)
-    	ip_addr = raw_input('Which IP do you want to connect to?\n')
-    	if (ip_addr != 'N'):
-            client = Client(ip_addr)
-            client.connect()
+def connectTo(): # I am the client!
+    global win, ip, client
+    ip = entry_ip.get()
+    result = client.connect(ip) # Initiate video chat as client
+    if (result != None):
+        tkMessageBox.showerror("Error", "Nobody there!")
+
+daemon = Daemon()
+server = Server()
+client = Client()
+
+thread_daemon = Thread(target = daemon.start)
+thread_daemon.start()
+
+# Tkinter stuff
+root = tk.Tk()
+root.title("Chat Client")
+win = tk.Toplevel()
+ip = ''
+entry_ip = tk.Entry(win) # IP entry
+button_connect = tk.Button(win, text = 'Connect To', command = lambda: connectTo()) # Connect to IP
+entry_ip.pack(); button_connect.pack();
+root.withdraw()
+
+root.after(0, constantlyCheck)
+root.mainloop()
