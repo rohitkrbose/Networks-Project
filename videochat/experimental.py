c7862ae2709d5f44ee6ec2ae3ccf302399749b1b
Rohit Bose <rohit.kr.bose@gmail.com>
2018-11-14 11:09:17 +0530

    Using

--- videochat/experimental.py
+++ videochat/experimental.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process,Queue,Pipe
 import socket, videosocket
 from videofeed import VideoFeed
 from threading import Thread,Timer
@@ -18,17 +19,17 @@ class Daemon:
         global haveConnection, U_client_socket, U_address
         while True:
             cs, addr = self.daemon_socket.accept()
-            if (haveConnection == True):
+            if (haveConnection == True): # if it is already engaged with someone, ignore the new request
                 continue
-            haveConnection = True
-            U_client_socket = cs; U_address = addr;
+            haveConnection = True # if it is not engaged, set this flag variable to true
+            U_client_socket = cs; U_address = addr; # store client_socket and client address in global variables, as this is a thread
             print "I got a connection from ", addr
 
 class Server:
     def connect(self):
         global haveConnection, U_client_socket, U_address
-        client_socket, address = U_client_socket, U_address
-        vsock = videosocket.videosocket(client_socket)
+        client_socket, address = U_client_socket, U_address # retrieve info from global variables (changes made by Daemon)
+        vsock = videosocket.videosocket(client_socket) # establish a video connection
         videofeed = VideoFeed(1,"A1",1)
         while True:
             frame = vsock.vreceive()
@@ -43,9 +44,9 @@ class Client:
         try:
             client_socket.connect((ip_addr, 6000))
         except:
-            return ('Unavailable')
-        win.withdraw()
-        vsock = videosocket.videosocket (client_socket)
+            return ('Unavailable') # if Client can't get a connection to that IP
+        win.withdraw() # Hide the Connect To window
+        vsock = videosocket.videosocket (client_socket) # establish a video connection
         videofeed = VideoFeed(1,"A2",1)
         while True:
             frame = videofeed.get_frame()
@@ -53,18 +54,19 @@ class Client:
             frame = vsock.vreceive()
             videofeed.set_frame(frame)
 
-def constantlyCheck (): # I am the server!
+def constantlyCheck (): # I am the server! This is a helper function for the daemon.
     global haveConnection, server, win
-    if (haveConnection == True):
-        win.withdraw()
+    # Here we should have an option: To reject or to accept. Only accept code is written here.
+    if (haveConnection == True): # If daemon listened to some shit
+        win.withdraw()  # Hide the Connect To window
         haveConnection = False
-        server.connect()
-    root.after(3, constantlyCheck)
+        server.connect() # Initiate video chat as server
+    root.after(2, constantlyCheck) # Run this function again after 2 seconds
 
 def connectTo(): # I am the client!
     global win, ip, client
     ip = entry_ip.get()
-    result = client.connect(ip)
+    result = client.connect(ip) # Initiate video chat as client
     if (result != None):
         tkMessageBox.showerror("Error", "Nobody there!")
 
@@ -81,7 +83,7 @@ root.title("Chat Client")
 win = tk.Toplevel()
 ip = ''
 entry_ip = tk.Entry(win) # IP entry
-button_connect = tk.Button(win, text = 'Connect', command = lambda: connectTo()) # Connect to IP
+button_connect = tk.Button(win, text = 'Connect To', command = lambda: connectTo()) # Connect to IP
 entry_ip.pack(); button_connect.pack();
 root.withdraw()
 
