7cb05208a8640a1e828d17a3e0730453bd07cbc3
Rohit Bose <rohit.kr.bose@gmail.com>
2018-11-14 18:37:39 +0530

    Quit respectfully

--- videochat/serverclient.py
+++ videochat/serverclient.py
@@ -7,13 +7,12 @@ import StringIO
 import cv2
 
 haveConnection = False
-videoRunning = True
 U_client_socket = U_address = None
 
 class Daemon:
     def __init__(self):
         self.daemon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.daemon_socket.bind(("", 6000))
+        self.daemon_socket.bind(("", 6001))
         self.daemon_socket.listen(5)
         print "TCPServer Waiting for client on port 6001"
 
@@ -29,14 +28,16 @@ class Daemon:
 
 class Server:
     def connect(self):
-        global haveConnection, U_client_socket, U_address, videoRunning, quit_win
+        global haveConnection, U_client_socket, U_address
         client_socket, address = U_client_socket, U_address # retrieve info from global variables (changes made by Daemon)
         vsock = videosocket.videosocket(client_socket) # establish a video connection
         videofeed = VideoFeed(1,"Server",1)
-        quit_win.deiconify()
+        tkMessageBox.showerror("Info", "Press \'q\' to quit!")
         try:
-            while (videoRunning):
-                if (cv2.waitKey(0) == ord('c')):
+            while (True):
+                key=cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    win.deiconify()
                     break
                 frame = vsock.vreceive()
                 videofeed.set_frame(frame)
@@ -47,19 +48,21 @@ class Server:
 
 class Client:
     def connect(self, ip_addr = "127.0.0.1"):
-        global win, videoRunning, quit_win
+        global win
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            client_socket.connect((ip_addr, 6001))
+            client_socket.connect((ip_addr, 6000))
         except:
             return ('Unavailable') # if Client can't get a connection to that IP
         win.withdraw() # Hide the Connect To window
-        quit_win.deiconify()
+        tkMessageBox.showerror("Info", "Press \'q\' to quit!")
         vsock = videosocket.videosocket(client_socket) # establish a video connection
         videofeed = VideoFeed(1,"Client",1)
         try:
-            while (videoRunning):
-                if (cv2.waitKey(0) == ord('c')):
+            while (True):
+                key=cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    win.deiconify()
                     break
                 frame = videofeed.get_frame()
                 vsock.vsend(frame)
@@ -84,10 +87,6 @@ def connectTo(): # I am the client!
     if (result != None):
         tkMessageBox.showerror("Error", "Nobody there!")
 
-def changeVideoState ():
-    global videoRunning
-    videoRunning = False
-
 daemon = Daemon()
 server = Server()
 client = Client()
@@ -99,15 +98,11 @@ thread_daemon.start()
 root = tk.Tk()
 root.title("Chat Client")
 win = tk.Toplevel()
-quit_win = tk.Toplevel()
 ip = ''
 entry_ip = tk.Entry(win) # IP entry
 button_connect = tk.Button(win, text = 'Connect To', command = lambda: connectTo()) # Connect to IP
-button_quit = tk.Button(quit_win, text = 'Quit', command = lambda: changeVideoState()) # Alter video state variable
-button_quit.pack()
 entry_ip.pack(); button_connect.pack();
 root.withdraw()
-# quit_win.withdraw()
 
 root.after(0, constantlyCheck)
 root.mainloop()
